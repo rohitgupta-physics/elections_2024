2bb1561aeb1a7a532c0e212d58bd1e658f1d3520
Rohit Gupta <rohitcontact666@gmail.com>
2024-03-28 20:00:09 -0700

    Removed get_rating function from Pollster class

--- pollster_class.py
+++ pollster_class.py
@@ -5,23 +5,17 @@ import typing
 class Pollster:
 
     def __init__(self, rating_id: str) -> None:
-        _ids: list[str] = list()
-        with open("pollster_rating.csv") as file:
-            reader: csv.DictReader[str] = csv.DictReader(file)
-            row: dict[str, str]
-            for row in reader:
-                _ids.append(row["pollster_rating_id"])
-        if rating_id not in _ids:
-            raise ValueError("Not a valid pollster rating id")
-        else:
-            self.rating_id = rating_id
-
-    def get_rating(self) -> str:
+        self.rating_id: str = ""
+        self._data: dict[str, str] = dict()
         file: typing.TextIO
         with open("pollster_rating.csv") as file:
             reader: csv.DictReader[str] = csv.DictReader(file)
             row: dict[str, str]
             for row in reader:
-                if row["pollster_rating_id"] == self.rating_id:
-                    return row["numeric_grade"]
-        return "Not a valid pollster"
+                if row["pollster_rating_id"] == rating_id:
+                    self.rating_id = rating_id
+                    self._data = row
+                    break
+        if self.rating_id == "":
+            raise ValueError("Not a valid pollster rating id")
+        self.rating: str = self._data["numeric_grade"]
--- tests/test_pollster_class.py
+++ tests/test_pollster_class.py
@@ -4,11 +4,11 @@ import pytest
 
 def test_correct_input():
     yougov: Pollster = Pollster("391")
-    assert yougov.get_rating() == "2.9"
-    assert Pollster("253").get_rating() == "2.5"
+    assert yougov.rating == "2.9"
+    assert Pollster("253").rating == "2.5"
 
 
 def test_incorrect_input():
     with pytest.raises(ValueError):
         fake: Pollster = Pollster("10000")
-        fake.get_rating()
+        fake.rating
