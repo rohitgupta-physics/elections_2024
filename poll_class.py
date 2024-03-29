121ff1238450f067ed20b93a55903429a95710b3
Rohit Gupta <rohitcontact666@gmail.com>
2024-03-29 08:43:23 -0700

    Modified Poll class to take in modified csv files

--- poll_class.py
+++ poll_class.py
@@ -5,21 +5,24 @@ from pollster_class import Pollster
 
 class Poll:
 
-    def __init__(self, poll_id: str) -> None:
-        self.poll_id: str = ""
-        self._data: dict[str, str]
+    def __init__(self, new_poll_id: str, file_name: str) -> None:
+
+        if not file_name.endswith("modified.csv"):
+            raise ValueError("Not a modified csv file")
+
+        self.new_poll_id: str = ""
+        self.data: list[dict[str, str]]
         self.pollster: Pollster
 
         # checks if pollster id matches any of the pollster from general_election_biden_vs_trump.csv
         file: typing.TextIO
-        with open("general_election_biden_vs_trump.csv") as file:
+        with open(file_name) as file:
             reader: csv.DictReader[str] = csv.DictReader(file)
             row: dict[str, str]
             for row in reader:
-                if row["poll_id"] == poll_id:
-                    self.poll_id = poll_id
-                    self._data = row
+                if row["new_poll_id"] == new_poll_id:
+                    self.new_poll_id = new_poll_id
+                    self.data.append(row)
                     self.pollster = Pollster(row["pollster_rating_id"])
-                    break
-        if self.poll_id == "":
+        if self.new_poll_id == "":
             raise ValueError("Not a valid poll id")
