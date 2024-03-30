e6d9a62cc9105f43c87450d0097aad2f7ed85076
Rohit Gupta <rohitcontact666@gmail.com>
2024-03-30 09:38:51 -0700

    Added documentation to poll_class.py

--- poll_class.py
+++ poll_class.py
@@ -38,18 +38,31 @@ def get_poll_ids(csv_file: str) -> list[str]:
 
 class Poll:
     """
-    A class used to represent a poll.
+    A class used to represent a poll. If there are multiple polling questions asked by a
+    pollster they are treated as distinct polls.
 
     ...
 
     Attributes
-    ------------
+    ----------
+    new_poll_id: str
+        A poll id used to identify the poll in the csv file
+    filename: str
+        A csv file that contains poll data whose name ends with modified.csv
 
+    Methods
+    -------
+    get_pollster_name() -> str
+        Returns the name of the pollster
+    get_candidate_names() -> list[str]
+        Returns the names of the candidates polled
+    get_polling_numbers() -> dict[str, float]
+        Returns a dictionary with candidate name as keys and their polling numbers as values
     """
 
-    def __init__(self, new_poll_id: str, file_name: str) -> None:
+    def __init__(self, new_poll_id: str, filename: str) -> None:
 
-        if not file_name.endswith("modified.csv"):
+        if not filename.endswith("modified.csv"):
             raise ValueError("Not a modified csv file")
 
         self.new_poll_id: str = ""
@@ -58,7 +71,7 @@ class Poll:
 
         # adds all the polling data that matches the given poll id and raised a ValueError if the poll id is not present in the csv file
         file: typing.TextIO
-        with open(file_name) as file:
+        with open(filename) as file:
             reader: csv.DictReader[str] = csv.DictReader(file)
             row: dict[str, str]
             for row in reader:
@@ -70,12 +83,36 @@ class Poll:
             raise ValueError("Not a valid poll id")
 
     def get_pollster_name(self) -> str:
+        """Returns the name of the pollster from the pollster_rating.csv file
+
+        Returns
+        -------
+        str
+            Name of the pollster e.g. "The New York Times/Siena College"
+        """
+
         return self.pollster.get_name()
 
     def get_candidate_names(self) -> list[str]:
+        """List of candidate names which are included in poll
+
+        Returns
+        -------
+        list[str]
+            A list containing candidate names like ["Joe Biden", ...]
+        """
+
         return [row["candidate_name"] for row in self._data]
 
     def get_polling_numbers(self) -> dict[str, float]:
+        """Polling data corresponding to a given poll
+
+        Returns
+        -------
+        dict[str, float]
+            Eg {"Joe Biden": 42.0, "Donald Trump": 43.0}
+        """
+
         ans: dict[str, float] = dict()
         row: dict[str, str]
         for row in self._data:
